a3e7c7c1817a9dab24544736d6d1af60626ad732
Constantinos Eracleous <constantinose@imlmarketing.local>
2018-04-23 12:53:51 +0300

    finished popup modal

--- client/src/app/components/item-details/item-details.component.ts
+++ client/src/app/components/item-details/item-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Item } from '../../models/Item';
 import { ActivatedRoute } from '@angular/router';
 import { ItemsService } from '../../services/items.service';
@@ -15,13 +15,8 @@ export class ItemDetailsComponent implements OnInit {
   public item: Item = <Item>{};
   private routerSubscription: Subscription;
 
-  closeResult: string;
-
-  showModal(content) {
-    this.modalService.open(content, {
-      centered: true,
-      size: 'lg'
-    });
+  open(content) {
+    this.modalService.open(content, { centered: true, size: 'lg' });
   }
 
   public constructor(
