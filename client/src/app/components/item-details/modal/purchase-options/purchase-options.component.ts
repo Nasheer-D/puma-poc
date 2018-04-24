d21d59397cf6eedceaeb75f2f6c80ca61f8f8f27
Constantinos Eracleous <constantinose@imlmarketing.local>
2018-04-24 12:24:33 +0300

    Remoced unecessary imports

--- client/src/app/components/item-details/modal/purchase-options/purchase-options.component.ts
+++ client/src/app/components/item-details/modal/purchase-options/purchase-options.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import { Item } from '../../../../models/Item';
-import { ActivatedRoute } from '@angular/router';
-import { ItemsService } from '../../../../services/items.service';
-import { HttpResponse } from '../../../../utils/web/models/HttpResponse';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Subscription } from 'rxjs/Subscription';
-import { isInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-purchase-options',
