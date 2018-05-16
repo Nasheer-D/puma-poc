0e9546a04185df0ee1c807b15081a3bd7b90f865
Constantinos Eracleous <constantinos@pumapay.io>
2018-05-16 13:57:39 +0300

    updates unit tests

--- server/test/unit/pumapay/SignatureCalculator.spec.ts
+++ server/test/unit/pumapay/SignatureCalculator.spec.ts
@@ -29,23 +29,24 @@ describe('SignatureCalculator with correct transaction data', () => {
 
 describe('SignatureCalulator with wrong transaction data', () => {
   const signatureCalculator = new SignatureCalculator(transaction);
+  
   it('should return an error when address has no prefix', () => {
     transaction.to = 'aA5CBc1eB612e9A6FB7DCA72f559c8BC3a63F510';
     const signature = signatureCalculator.calculate();
-    console.log(signature);
-    expect(signature.[Error]).to.equal('Error: hex string must have 0x prefix (arg="value", value="aA5CBc1eB612e9A6FB7DCA72f559c8BC3a63F510")');
+    expect(signature).to.have.any.keys('arg','code','reason','value','Error: hex string must have 0x prefix (arg="value", value="aA5CBc1eB612e9A6FB7DCA72f559c8BC3a63F510")');
   });
   it('should return an error when callback has the wrong datatype', () => {
     transaction.callback = 2;
     transaction.to = '0xaA5CBc1eB612e9A6FB7DCA72f559c8BC3a63F510';
     const signature = signatureCalculator.calculate();
-    expect(signature).to.equal('TypeError: "value" argument must not be a number');
+    console.log('typeError:::', signature);
+    expect(signature).to.include.keys('TypeError: "value" argument must not be a number');
   });
   it('should return an error when name has the wrong datatype', () => {
     transaction.name = 2;
     transaction.callback = 'asd';
     transaction.to = '0xaA5CBc1eB612e9A6FB7DCA72f559c8BC3a63F510';
     const signature = signatureCalculator.calculate();
-    expect(signature).to.equal('TypeError: "value" argument must not be a number');
+    expect(signature).to.have.any.keys('arg','code','reason','value','TypeError: "value" argument must not be a number');
   });
 });
