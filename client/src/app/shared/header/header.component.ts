1134f20e0ce4a461ebfd5e24a9c8c798df5f25e7
Constantinos Eracleous <constantinos@pumapay.io>
2018-05-17 10:57:52 +0300

    removed header changes

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../models/User';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-  public users: User = <User>{};
-  public ngOnInit() {
-    this.users = JSON.parse(localStorage.getItem('currentUser'));
-    console.log('User:::', this.users);
-  }
-}
+export class HeaderComponent {}
