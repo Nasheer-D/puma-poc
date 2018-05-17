ae19cb38a0418289f479ce834af67c94412543b3
Constantinos Eracleous <constantinos@pumapay.io>
2018-05-17 13:02:04 +0300

    fixed users

--- db/insert-users.sql
+++ db/insert-users.sql
@@ -1,15 +1,10 @@
 INSERT INTO public.app_users(
-	"userID", "userName", email, hash, salt, credits, "registrationDate")
-	VALUES ('owner01', 'owner01', 'owner01@pumapay.io', 'b0601dbc5a68860fc1a839afc44c881b0e74611c9d70c0be4151aae40f27058fda4c5e1399a9de2442ec76f6380094913d36d63a8989c699d8a37ebf64ed4d2b', '2f685b8c15383517d7e57e3823bcde13', 1000000, 1522758956);
+	"userID", "userName", email, hash, salt, credits, "registrationDate", "walletAddress")
+	VALUES ('owner01', 'owner01', 'owner01@pumapay.io', 'b0601dbc5a68860fc1a839afc44c881b0e74611c9d70c0be4151aae40f27058fda4c5e1399a9de2442ec76f6380094913d36d63a8989c699d8a37ebf64ed4d2b', '2f685b8c15383517d7e57e3823bcde13', 1000000, 1522758956,'0xb344ec617313d90331285E33cF4168DDb5C91B21');
 
 INSERT INTO public.app_users(
-	"userID", "userName", email, hash, salt, credits, "registrationDate")
-	VALUES ('owner02', 'owner02', 'owner02@pumapay.io', 'b0601dbc5a68860fc1a839afc44c881b0e74611c9d70c0be4151aae40f27058fda4c5e1399a9de2442ec76f6380094913d36d63a8989c699d8a37ebf64ed4d2b', '2f685b8c15383517d7e57e3823bcde13', 1500000, 1522758956);
-
+	"userID", "userName", email, hash, salt, credits, "registrationDate", "walletAddress")
+	VALUES ('owner02', 'owner02', 'owner02@pumapay.io', 'b0601dbc5a68860fc1a839afc44c881b0e74611c9d70c0be4151aae40f27058fda4c5e1399a9de2442ec76f6380094913d36d63a8989c699d8a37ebf64ed4d2b', '2f685b8c15383517d7e57e3823bcde13', 1500000, 1522758956,'0xCb9031894b5736E4D32EB18a19eC6857e604B35a');
 INSERT INTO public.app_users(
-	"userID", "userName", email, hash, salt, credits, "registrationDate")
-	VALUES ('user01', 'user01', 'user01@pumapay.io', 'b0601dbc5a68860fc1a839afc44c881b0e74611c9d70c0be4151aae40f27058fda4c5e1399a9de2442ec76f6380094913d36d63a8989c699d8a37ebf64ed4d2b', '2f685b8c15383517d7e57e3823bcde13', 2000000, 1522758956);
-
-INSERT INTO public.app_users(
-	"userID", "userName", email, hash, salt, credits, "registrationDate")
-	VALUES ('user02', 'user02', 'user02@pumapay.io', 'b0601dbc5a68860fc1a839afc44c881b0e74611c9d70c0be4151aae40f27058fda4c5e1399a9de2442ec76f6380094913d36d63a8989c699d8a37ebf64ed4d2b', '2f685b8c15383517d7e57e3823bcde13', 3000000, 1522758956);
+	"userID", "userName", email, hash, salt, credits, "registrationDate", "walletAddress")
+	VALUES ('owner03', 'owner03', 'owner03@pumapay.io', 'b0601dbc5a68860fc1a839afc44c881b0e74611c9d70c0be4151aae40f27058fda4c5e1399a9de2442ec76f6380094913d36d63a8989c699d8a37ebf64ed4d2b', '2f685b8c15383517d7e57e3823bcde13', 1500000, 1522758956,'0x9d11DDd84198B30E56E31Aa89227344Cdb645e34');
