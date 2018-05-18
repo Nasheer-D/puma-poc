45a37740a36d39aa91407ba2ea4a4dd302b7cd20
Constantinos Eracleous <constantinos@pumapay.io>
2018-05-18 14:35:25 +0300

    package modal now pulls packages fro service

---
+++ client/src/app/models/Packages.ts
@@ -0,0 +1,9 @@
+export interface Package {
+    packageID: string;
+    amount: number;
+    bonusCredits: number;
+    bonusTickets: number;
+    featured: boolean;
+    priceInUSD: number;
+    priceInPMA: number;
+}
