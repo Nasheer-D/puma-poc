4c51373ee5ec4cf0018be55411341c1412a0534b
Stella Evagorou <stella@pumapay.io>
2018-05-18 10:45:27 +0300

    Show purchase credit modal

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../models/User';
+import { PurchasePackagesComponent } from '../../components/item-details/modals/purchase-packages/purchase-packages.component';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('purchasePackages')
+  public purchasePackages: PurchasePackagesComponent;
   public user: User;
 
   public ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('currentUser'));
+  }
+  public showCreditModal() {
+    this.purchasePackages.open();
   }
 }
