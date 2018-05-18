e1eabede9385e0bfe2bdc1215c4eaa737bd4be5c
Constantinos Eracleous <constantinos@pumapay.io>
2018-05-18 11:05:07 +0300

    created db table/insert data

---
+++ db/insert-packages.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.credit_packages(
+	"packageID", amount, "bonusCredits", "bonusTickets", featured)
+	VALUES ('package01' , 10, 0, 0, false);
+
+INSERT INTO public.credit_packages(
+	"packageID", amount, "bonusCredits", "bonusTickets", featured)
+	VALUES ('package02' , 25, 2, 1, false);
+
+INSERT INTO public.credit_packages(
+	"packageID", amount, "bonusCredits", "bonusTickets", featured)
+	VALUES ('package03' , 50, 10, 2, true);        
+
+INSERT INTO public.credit_packages(
+	"packageID", amount, "bonusCredits", "bonusTickets", featured)
+	VALUES ('package04' , 100, 20, 4, false);    
