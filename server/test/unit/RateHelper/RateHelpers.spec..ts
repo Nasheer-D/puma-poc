a43fb104d16d45138b7da4e10a55706a8b8a3c79
Stella Evagorou <stella@pumapay.io>
2018-05-21 11:45:43 +0300

    DS-21 unit test

---
+++ server/test/unit/RateHelper/RateHelpers.spec..ts
@@ -0,0 +1,14 @@
+import * as chai from 'chai';
+import { RateHelpers } from '../../../../server/src/utils/rateHelpers/RateHelper';
+const expect = chai.expect;
+
+let rateHelpers = new RateHelpers()
+const rate = 0.015; 
+
+describe('A RateHelpers', () => {
+
+    it('should return rate', () => {
+        // expect(rate).to.equal(rate);
+        expect(rateHelpers.getPMAtoUSDRate()).equals(rate);
+    });
+})
