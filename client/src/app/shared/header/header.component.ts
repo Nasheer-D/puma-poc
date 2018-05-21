96f340358b53f2384c1387f50012852ba8af246e
Stella Evagorou <stella@pumapay.io>
2018-05-21 17:11:14 +0300

    DS-10-clear-session

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../models/User';
 import { PurchasePackagesComponent } from '../../components/item-details/modals/purchase-packages/purchase-packages.component';
+import { AuthenticationService } from '../../services/authentication.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
+  providers: [AuthenticationService]
+
 })
 export class HeaderComponent implements OnInit {
   @ViewChild('purchasePackages')
   public purchasePackages: PurchasePackagesComponent;
   public user: User;
 
+  constructor(private authservice: AuthenticationService) {
+  }
+
   public ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('currentUser'));
   }
+
   public showCreditModal() {
     this.purchasePackages.open();
+  }
+
+  public callLogoutFunction() {
+    this.authservice.logout();
   }
 }
