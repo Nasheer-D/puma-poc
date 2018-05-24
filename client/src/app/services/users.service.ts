b5fc621b36a41189a146a3d0f0929e84e0013382
Stella Evagorou <stella@pumapay.io>
2018-05-24 17:56:33 +0300

    DS-18 retrieve user credit

---
+++ client/src/app/services/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpGetRequest } from '../utils/web/HttpGetRequest';
+import { AuthenticationService } from './authentication.service';
+import { Constants } from '../app.constants';
+import { User } from '../models/User';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class UserService {
+  private actionUrl: string;
+
+  public constructor(private http: HttpClient, private authService: AuthenticationService) {
+    this.actionUrl = `${Constants.apiHost}${Constants.apiPrefix}user/`;
+  }
+
+  public getLoggedInUserCredits() {
+    return new HttpGetRequest(this.http, `${this.actionUrl}credits`).getResult();
+  }
+}
